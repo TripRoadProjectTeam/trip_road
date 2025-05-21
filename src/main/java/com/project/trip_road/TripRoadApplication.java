8985a2715fccb704afbd365856470fc878a3dfc0
kael0595 <changgyu549@gmail.com>
2025-05-21 20:14:38 +0900

    git init

---
+++ src/main/java/com/project/trip_road/TripRoadApplication.java
@@ -0,0 +1,13 @@
+package com.project.trip_road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripRoadApplication.class, args);
+	}
+
+}
