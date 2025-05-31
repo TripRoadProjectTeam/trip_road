5f9dacd739aeefd3cd3e08ed72c312d3a71268f7
kael0595 <changgyu549@gmail.com>
2025-05-31 14:44:20 +0900

    메인컨트롤러 & 메인페이지 작성

---
+++ src/main/java/com/project/trip_road/MainController.java
@@ -0,0 +1,13 @@
+package com.project.trip_road;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
--- src/main/java/com/project/trip_road/base/configuration/SecurityConfig.java
+++ src/main/java/com/project/trip_road/base/configuration/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.project.trip_road.base.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/", "/member/login", "/member/join", "/post/list","/post/detail/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/member/login")
+                        .defaultSuccessUrl("/"))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true));
+        return http.build();
+    }
 }
